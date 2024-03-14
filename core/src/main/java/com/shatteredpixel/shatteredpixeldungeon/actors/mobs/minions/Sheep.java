79c550c625f5c3e583bdf72d2be5c338a8347dda
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2024-03-15 02:31:40 +0700

    ReInc 0.3.0: implemented sheep minion:
    - no longer has tanky restriction due to minion rework
    - ranks no longer trade minion slots for more defense
    - rank II now allows sheep to reflect damage
    - rank III makes sheep have effectively infinity priority for slightly higher stats

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/minions/Sheep.java
@@ -0,0 +1,65 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2023 Evan Debenham
+ *
+ * Summoning Pixel Dungeon Reincarnated
+ * Copyright (C) 2023-2024 Trashbox Bobylev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.actors.mobs.minions;
+
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.items.stones.StoneOfAggression;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.SheepTankSprite;
+import com.watabou.utils.Random;
+
+public class Sheep extends Minion {
+    {
+        spriteClass = SheepTankSprite.class;
+    }
+
+    @Override
+    protected boolean act() {
+        boolean actResult = super.act();
+        if (rank == 3){
+            Buff.affect(this, StoneOfAggression.Aggression.class, 2f);
+        }
+        return actResult;
+    }
+
+    @Override
+    public int defenseProc(Char enemy, int damage) {
+        if (rank == 2){
+            int effectiveDamage = Random.NormalIntRange(0, (int) Math.floor(damage*3/4f));
+
+            if (enemy.sprite != null) {
+                hitSound(Random.Float(0.87f, 1.15f));
+                enemy.sprite.bloodBurstA(sprite.center(), effectiveDamage);
+                enemy.sprite.flash();
+            }
+        }
+        return super.defenseProc(enemy, damage);
+    }
+
+    @Override
+    public float targetPriority() {
+        return super.targetPriority()*2f;
+    }
+}
---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/staffs/SheepStaff.java
@@ -0,0 +1,57 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2023 Evan Debenham
+ *
+ * Summoning Pixel Dungeon Reincarnated
+ * Copyright (C) 2023-2024 Trashbox Bobylev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.items.staffs;
+
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.minions.GrayRat;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.minions.Minion;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.minions.Sheep;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+
+public class SheepStaff extends Staff {
+    {
+        image = ItemSpriteSheet.WOOLY_STAFF;
+        minionType = Sheep.class;
+        tier = 2;
+        chargeTurns = 575;
+        table = new BalanceTable(
+                55, 14, 0, 0, 1, 1,
+                45, 12, 0, 0, 0, 0,
+                65, 16, 0, 0, 2, 1);
+    }
+
+    @Override
+    public Minion.BehaviorType defaultBehaviorType() {
+        return Minion.BehaviorType.AGGRESSIVE;
+    }
+
+    @Override
+    public void customizeMinion(Minion minion) {
+        switch (rank()){
+            case 1: minion.minDefense = 2; minion.maxDefense = 6; break;
+            case 2: minion.minDefense = 1; minion.maxDefense = 4; break;
+            case 3: minion.minDefense = 3; minion.maxDefense = 8; break;
+        }
+    }
+}
