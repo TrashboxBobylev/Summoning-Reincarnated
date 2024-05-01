3c715d9c9a74506fa57adba664cdd36969eb14be
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2024-05-02 01:05:52 +0700

    ReInc 0.3.0: added changelog for 0.3.0

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/vReInc_Changes.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/vReInc_Changes.java
@@ -8,13 +8,19 @@ import com.shatteredpixel.shatteredpixeldungeon.items.armor.ScoutArmor;
 import com.shatteredpixel.shatteredpixeldungeon.items.bombs.Bomb;
 import com.shatteredpixel.shatteredpixeldungeon.items.bombs.Firebomb;
 import com.shatteredpixel.shatteredpixeldungeon.items.food.Food;
+import com.shatteredpixel.shatteredpixeldungeon.items.quest.CleanWater;
+import com.shatteredpixel.shatteredpixeldungeon.items.spells.EnchantParchment;
+import com.shatteredpixel.shatteredpixeldungeon.items.staffs.*;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.SpiritBow;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Cleaver;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Quarterstaff;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.RunicBlade;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.shop.StoneHammer;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.ChangesScene;
-import com.shatteredpixel.shatteredpixeldungeon.sprites.HeroSprite;
-import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.*;
+import com.shatteredpixel.shatteredpixeldungeon.ui.BuffIcon;
+import com.shatteredpixel.shatteredpixeldungeon.ui.BuffIndicator;
 import com.shatteredpixel.shatteredpixeldungeon.ui.Icons;
 import com.shatteredpixel.shatteredpixeldungeon.ui.Window;
 import com.watabou.noosa.Image;
@@ -23,9 +29,195 @@ import java.util.ArrayList;
 
 public class vReInc_Changes {
     public static void addAllChanges( ArrayList<ChangeInfo> changeInfos ){
-
+        add_v0_3_0_Changes(changeInfos);
         add_v0_2_0_Changes(changeInfos);
         add_v0_1_0_Changes(changeInfos);
+    }
+
+    public static void add_v0_3_0_Changes( ArrayList<ChangeInfo> changeInfos ){
+        ChangeInfo changes = new ChangeInfo("vReInc-0.3.0", true, "");
+        changes.hardlight(Window.TITLE_COLOR);
+        changeInfos.add(changes);
+
+        changes.addButton( new ChangeButton(Icons.get(Icons.BOBYLEV), "Developer Commentary",
+                "_-_ Released May 1st, 2024\n" +
+                        "_-_ 116 days after Reincarnated 0.2.0\n\n" +
+                        "It's been a long wait, isn't it? The 3 months long development hell was surely something for mental state and other stuff... " +
+                        "But at least the main course of action, minions, are finally here!"));
+
+        changes.addButton( new ChangeButton(Icons.get(Icons.SHPX), "Shattered Ports",
+                "Implemented Shattered v2.3.2 changes."));
+
+        changes = new ChangeInfo("Minions Overhaul", false,
+                "Only 8 staffs have been implemented into the game, more will come with future updates.");
+        changes.hardlight(Window.TITLE_COLOR);
+        changeInfos.add(changes);
+
+        changes.addButton( new ChangeButton(new ItemSprite(ItemSpriteSheet.STAFF_PLACEHOLDER), "In general",
+                "_-_ Removed attunement \"minion slots\"; now each minion corresponds to their own staff, that tracks their health.\n" +
+                        "_-_ Now have _4 behavior types_, that tell them what enemies to hunt:\n" +
+                        "   * _Reactive_ minions will target enemies that are hit by hero\n" +
+                        "   * _Protective_ minions will target enemies that attack hero\n" +
+                        "   * _Aggressive_ minions will target any enemies they see, even sleeping ones\n" +
+                        "   * _Passive_ minions will not attack anything, but slowly regenerate their health\n\n" +
+                        "_-_ Behavior types can be switched after summoning the minion with quick-using the staff.\n" +
+                        "_-_ Attunement now acts as magical equivalent of strength, gating stronger staffs and making earlier staffs more powerful with progression.\n" +
+                        "_-_ Sentries no longer use their health to attack, they now have _Sentry's Resource_ effect which tracks how many actions the sentry has left.\n" +
+                        "_-_ Sentries can be moved around with quick-use, but this consumes sentry's Resource points.\n" +
+                        "_-_ Staffs no longer recharge when minions are present."));
+
+        changes.addButton(new ChangeButton(new GrayRatStaff(),
+                "_-_ Resprited both staff and minion to be more distinct.\n" +
+                        "_-_ HP changed from _35/23/12_ to _35/30/10_.\n" +
+                        "_-_ Damage changed from _5-12/10-16/15-25_ to _3-14/3-11/1-8_.\n" +
+                        "_-_ No longer gets more armor with ranks; armor changed to _1-3_.\n" +
+                        "_-_ _Rank II_ now heals itself when attacking and reduces player's hunger usage, when passive.\n" +
+                        "_-_ _Rank III_ is equivalent of normal rat, but staff's recharge is reduced to 100 turns and rat gets 7.5 turns of adrenaline on summoning."));
+
+        changes.addButton(new ChangeButton(new SheepStaff(),
+                "_-_ No longer considered \"tanky\" minion, but has higher target priority over other minions.\n" +
+                        "_-_ HP changed from _50/50/50_ to _55/45/93_.\n" +
+                        "_-_ Recharge changed from _500 turns_ to _575 turns_.\n" +
+                        "_-_ No longer gets more armor with ranks; armor changed to _2-6/1-4/0_.\n" +
+                        "_-_ _Rank II_ now reflects damage back at the attacker.\n" +
+                        "_-_ _Rank III_ recharges for 862 turns and permanently has Targeted debuff on it, making all enemies engage such sheep when possible."));
+
+        changes.addButton(new ChangeButton(new MagicMissileStaff(),
+                "_-_ HP changed from _85/125/160_ to _40/55/20_.\n" +
+                        "_-_ Damage changed from _12-24/12-24/12-24_ to _4-15/10-30/3-12_.\n" +
+                        "_-_ Maximum resource changed from _85/42/40_ to _25/25/3_.\n" +
+                        "_-_ No longer gains AoE with ranks.\n" +
+                        "_-_ Now has additional halo effects on successful hits.\n" +
+                        "_-_ _Rank II_ has higher damage, that is affected by armor and less accurate, like YAPD's magic missile.\n" +
+                        "_-_ _Rank III_ is short-living sentry, that recharges for 75 turns and gives wand recharge on hitting enemies."));
+
+        changes.addButton(new ChangeButton(new GnollHunterStaff(),
+                "_-_ No longer always pierces armor.\n" +
+                        "_-_ HP changed from _25/40/66_ to _25/30/25_.\n" +
+                        "_-_ Damage changed from _6-12/7-12/9-12_ to _5-15/1-5/20-45_.\n" +
+                        "_-_ Now runs away from enemies more consistently.\n" +
+                        "_-_ No longer throws more darts per turn with ranks.\n" +
+                        "_-_ _Rank II_ now throws 3 darts per turn, that deal low damage, but trigger gnoll's enchantment more frequently.\n" +
+                        "_-_ _Rank III_ uses sniper shots, that pierce armor and knock enemies away, but trigger cooldown for each attack."));
+
+        changes.addButton(new ChangeButton(new WizardStaff(),
+                "_-_ Resprited both staff and minion to be more distinct.\n" +
+                        "_-_ HP changed from _35/28/19_ to _35/25/35_.\n" +
+                        "_-_ Damage changed from _4-15/2-8/0-5_ to _7-17/2-6/0_.\n" +
+                        "_-_ Recharge changed from _700/500/300 turns_ to _600/850/750 turns_.\n" +
+                        "_-_ His magic resistance is now capped at 25% and no longer changes with ranks.\n" +
+                        "_-_ _Rank II_ now attacks twice as frequently and attempts to corrupt the target enemy; the potency scales with progression.\n" +
+                        "_-_ _Rank III_ doesn't attack at all, but provides various boosts to player, that depend on wizard's behavior type."));
+
+        changes.addButton(new ChangeButton(new RoboStaff(),
+                "_-_ No longer considered \"tanky\" minion, but has higher target priority over other minions.\n" +
+                        "_-_ HP changed from _120/175/225_ to _120/105/215_.\n" +
+                        "_-_ Damage changed from _25-50/30-55/35-60_ to _20-40/15-25/5-15_.\n" +
+                        "_-_ Recharge changed from _675/875/1175 turns_ to _600/800/1500 turns_.\n" +
+                        "_-_ Attack delay changed from _3.5/2.5/1.5 turns_ to _2/1.5/1 turns_.\n" +
+                        "_-_ Now has 40% evasion penalty.\n" +
+                        "_-_ _Rank II_ has lower stats, but can chain to itself any kind of enemy, not just ranged ones.\n" +
+                        "_-_ _Rank III_ has lower armor, damage and passive regen rate, but heals from both attacking and being attacked."));
+
+        changes.addButton(new ChangeButton(new GooStaff(),
+                "_-_ HP changed from _45/55/65_ to _45/55/15_.\n" +
+                        "_-_ Damage changed from _20-35/15-26/8-17_ to _20-35/12-25/4-15_.\n" +
+                        "_-_ Charged attacks no longer charge for longer and become more deadly with ranks. They also no longer damage allies.\n" +
+                        "_-_ Armor is capped at _5-20_ and no longer increases with ranks.\n" +
+                        "_-_ _Rank II_ charged attack no longer deals damage, but covers larger range and applies effect based on a potion infused into Goo minion. Potions can be infused by throwing them into the minion.\n" +
+                        "_-_ _Rank III_ recharges for 200 turns and takes 10% damage without viscosity; its charged attack does damage based on depth's bomb damage and makes minion instantly die after using it."));
+
+        changes.addButton(new ChangeButton(new BlasterStaff(),
+                "_-_ HP changed from _120/160/200_ to _85/75/100_.\n" +
+                        "_-_ Damage changed from _15-60/12-40/10-34_ to _20-50/6-14/30-75_.\n" +
+                        "_-_ Maximum resource changed from _60/27/25_ to _18/50/12_.\n" +
+                        "_-_ Attack delay changed from _1.75/1.2/0.66 turns_ to _2/0.33/2.5 turns_.\n" +
+                        "_-_ No longer damages allies with its beams.\n" +
+                        "_-_ _Rank II_ rapidly fires weak beams, that deal more damage the more enemies they penetrate.\n" +
+                        "_-_ _Rank III_ recharges for 700 turns and deals huge KARMA damage, that ignores enemy's magic armor and ability to snap out of paralysing effects, and marks it with KARMA effect, which permanently makes enemy take 15% more damage from everything." +
+                        "As price, Rank III blaster can only strike each enemy once."));
+
+        changes = new ChangeInfo(Messages.get(ChangesScene.class, "changes"), false, null);
+        changes.hardlight(CharSprite.WARNING);
+        changeInfos.add(changes);
+
+        changes.addButton( new ChangeButton(new ItemSprite(ItemSpriteSheet.ELIXIR_ATTUNED), "Elixir of Attunement",
+                "Reimplemented Elixir of Attunement with some important changes:\n\n" +
+                        "_-_ Resprited.\n" +
+                        "_-_ No longer sold in shops.\n" +
+                        "_-_ No longer possible to create with alchemy.\n" +
+                        "_-_ Now has its own room with custom blue carpet tiles."));
+
+        changes.addButton( new ChangeButton(new RatSprite(), "Hordes",
+                "Reimplemented hordes.\n\n" +
+                        "_-_ Should be less glitchy or more glitchy, idk."));
+
+        changes.addButton( new ChangeButton(new AttunementConstructSprite(), "Attunement Construct",
+                "Reimplemented Final Froggits, with flavor change into Attunement Constructs.\n\n" +
+                        "_-_ Reduced some stats.\n" +
+                        "_-_ Spawn in less quantities in Demon Halls.\n" +
+                        "_-_ Does not spawn on Yog-Dzewa yet."));
+
+        changes.addButton( new ChangeButton(new ItemSprite(ItemSpriteSheet.ELIXIR_DRAGON), "Elemental Elixirs/Brews",
+                "Reimplemented merge of Blizzard Brew/Elixir of Icy Touch and Inferno Brew/Elixir of Dragon's Blood."));
+
+        changes.addButton( new ChangeButton(new ItemSprite(ItemSpriteSheet.RANK_MANAGER), "New way to obtain Ranks",
+                "Changed ranking from being changed with upgrades to to be managed by new item, Rank Manager.\n\n" +
+                        "_-_ Rank managers spawn every 2 depths, but not on boss depths, meaning there is 10 rank managers in entire game.\n" +
+                        "_-_ Rank managers allow to switch applicable item's rank to any of three, from I to III, and preview rank's effects on item."));
+
+        changes.addButton( new ChangeButton(new SpiritBow(),
+                "Reimplemented Spirit Bow's ability to have ranks.\n\n" +
+                        "_-_ Increased bow II's cone from 50 to 65 degrees.\n" +
+                        "_-_ Super-shot cooldown scales with bow's rank attack modifier."));
+
+        changes.addButton( new ChangeButton(new Food(),
+                "Adjusted satiety consumption across the board:\n\n" +
+                        "_-_ Decreased moving satiety value from _1.33_ to _1.25_.\n" +
+                        "_-_ Increased unlocking satiety value from _3.75_ to _5_.\n" +
+                        "_-_ Decreased item picking satiety value from _2.25_ to _1.75_.\n" +
+                        "_-_ Decreased throwing satiety value from _3_ to _2.5_.\n" +
+                        "_-_ Decreased equipping satiety value from _17_ to _15_.\n" +
+                        "_-_ Decreased level moving satiety value from _30_ to _25_.\n" +
+                        "_-_ Decreased attacking satiety value from _3.75_ to _3_."));
+
+        changes.addButton( new ChangeButton(new ItemSprite(ItemSpriteSheet.POTION_CRIMSON), "Healing gases",
+                "Reimplemented gases produced by throwing Potion of Healing and Elixir of Honeyed Healing, with some changes:\n\n" +
+                        "_-_ Now hurt hero with Pharmacophobia challenge active.\n" +
+                        "_-_ Both gases now heal percentage amount of HP to characters inside of it (2% for PoH and 6.25% for EoHH).\n" +
+                        "_-_ Honey gas is less dense visually and lasts 20% more time.\n" +
+                        "_-_ Healing gas lasts 10% less time."));
+
+        changes.addButton( new ChangeButton(new Cleaver(),
+                "Reimplemented cleaver, with rework to its functionality:\n\n" +
+                        "_-_ Now T3 weapon instead of T2.\n" +
+                        "_-_ Now deals 50% more damage than sword, but takes 50% more time to swing.\n" +
+                        "_-_ Decreased accuracy penalty from 40% to 20%.\n" +
+                        "_-_ Now deals 2x damage to enemies with full HP."));
+
+        changes.addButton( new ChangeButton(new EnchantParchment(),
+                "Reimplemented Enchant Transfer spell.\n\n" +
+                        "_-_ Now has recipe with stone of enchantment."));
+
+        changes.addButton( new ChangeButton(new BuffIcon(BuffIndicator.FROSTBURN, true), "Frostburn effect",
+                "Reimplemented Frostburn and cold fire:\n\n" +
+                        "_-_ The way to inflict it for now are Wand of Frost and Frostburn Brew.\n" +
+                        "_-_ Wand of Frost does less damage but inflicts Frostburn.\n" +
+                        "_-_ Frostburn Brew is made from potions of Liquid Flame and Frost and acts like Liquid Flame, but spreads frost fire instead of normal fire."));
+
+        changes.addButton(new ChangeButton(Icons.get(Icons.PREFS), Messages.get(ChangesScene.class, "misc"),
+                "_-_ Reimplemented Mineral Water and Scimitar's rework, with no changes.\n" +
+                        "_-_ Staffs now have their own journal tab and identification badge.\n" +
+                        "_-_ Fixed inconsistent outlines for all Summoning-sourced sprites.\n" +
+                        "_-_ Added remains item for Adventurer, which adds one bonus talent point into tier chosen.\n" +
+                        "_-_ Added badges for acquiring certain amount of attunement.\n" +
+                        "_-_ Reimplemented about page, with updated icons and links for contributors.\n" +
+                        "_-_ Resprited Runic Blade's projectile."));
+
+        changes.addButton(new ChangeButton(new Image(Assets.Sprites.SPINNER, 144, 0, 16, 16), Messages.get(ChangesScene.class, "bugfixes"),
+                "Fixed the following bugs from original Summoning:\n" +
+                        "_-_ Enemies running from Flashbang's effect even if they are immovable entities.\n" +
+                        "_-_ Dying with Blessed Ankh and Become Ethereal conduct doesn't trigger the revive."));
     }
 
     public static void add_v0_2_0_Changes( ArrayList<ChangeInfo> changeInfos ){
@@ -34,7 +226,7 @@ public class vReInc_Changes {
         changeInfos.add(changes);
 
         changes.addButton( new ChangeButton(Icons.get(Icons.BOBYLEV), "Developer Commentary",
-                "_-_ Released January 6th, 2023\n" +
+                "_-_ Released January 6th, 2024\n" +
                         "_-_ 13 days after Reincarnated 0.1.0"));
 
         changes.addButton( new ChangeButton( new ItemSprite(new Firebomb()), "Enhanced bombs",
