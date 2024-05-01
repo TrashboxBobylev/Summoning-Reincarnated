e6138a2551c62dcb1fbd18205de69ea6d26dc27c
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2024-05-02 00:11:25 +0700

    ReInc 0.3.0: unidentified staffs now display correct stats and show the proper "too heavy" message

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/staffs/Staff.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/staffs/Staff.java
@@ -413,7 +413,7 @@ public abstract class Staff extends Item implements AttunementItem, Rankable {
                 info += "\n\n" + Messages.get(Staff.class, "too_heavy_uh");
             }
         } else {
-            info += "\n\n" + Messages.get(this, "stats_unknown", tier, ATUReq(0), minionMin(0), minionMax(0), hp(0));
+            info += "\n\n" + Messages.get(this, "stats_unknown", tier, ATUReq(0), minionMin(1), minionMax(1), hp(1));
             if (ATUReq(0) > Dungeon.hero.ATU()) {
                 info += "\n\n" + Messages.get(AttunementItem.class, "probably_too_heavy");
             }
