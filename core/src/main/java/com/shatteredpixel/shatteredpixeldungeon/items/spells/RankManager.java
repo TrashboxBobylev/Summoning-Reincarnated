19918a2fbb5d7735bdcc74798346e2f3ce7abeff
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2024-10-03 03:45:38 +0700

    ReInc 0.4: reworked rank manager's rank select window:
    - rank descriptions and rank choosing windows are now combined into one tabbed window
    - rank manager is now only consumed, when you actually switch the rank

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/spells/RankManager.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/spells/RankManager.java
@@ -24,26 +24,19 @@
 
 package com.shatteredpixel.shatteredpixeldungeon.items.spells;
 
-import com.shatteredpixel.shatteredpixeldungeon.Assets;
-import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Invisibility;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Belongings;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
-import com.shatteredpixel.shatteredpixeldungeon.effects.Enchanting;
 import com.shatteredpixel.shatteredpixeldungeon.items.Item;
 import com.shatteredpixel.shatteredpixeldungeon.items.Rankable;
 import com.shatteredpixel.shatteredpixeldungeon.items.bags.Bag;
 import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.InventoryScroll;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
-import com.shatteredpixel.shatteredpixeldungeon.sprites.HeroSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
-import com.shatteredpixel.shatteredpixeldungeon.ui.Icons;
-import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
 import com.shatteredpixel.shatteredpixeldungeon.windows.WndBag;
 import com.shatteredpixel.shatteredpixeldungeon.windows.WndOptions;
-import com.shatteredpixel.shatteredpixeldungeon.windows.WndTitledMessage;
-import com.watabou.noosa.audio.Sample;
+import com.shatteredpixel.shatteredpixeldungeon.windows.WndRankManager;
 
 // it is not an inventory spell code-wise, but it is usable like one;
 // we just need for it to behave like enchantment scroll, which means
@@ -84,6 +77,11 @@ public class RankManager extends Spell {
         } );
     }
 
+    public void reShowSelector(){
+        curItem = this;
+        GameScene.selectItem(itemSelector);
+    }
+
     @Override
     protected void onCast(Hero hero) {
         GameScene.selectItem( itemSelector );
@@ -110,84 +108,10 @@ public class RankManager extends Spell {
         public void onSelect(final Item item) {
 
             if (item instanceof Rankable){
-                curItem.detach(curUser.belongings.backpack);
-                GameScene.show(new RankManager.WndRankSelect(item));
-            } else {
-                ((RankManager)curItem).confirmCancellation();
+                GameScene.show(new WndRankManager(((RankManager)curItem), item));
             }
         }
     };
-
-    public static class WndRankSelect extends WndOptions {
-
-        private static Item rankedItem;
-
-        //used in PixelScene.restoreWindows
-        public WndRankSelect(){
-            this(rankedItem);
-        }
-
-        public WndRankSelect(Item item){
-            super(new ItemSprite(new RankManager()),
-                    Messages.titleCase(new RankManager().name()),
-                    Messages.get(RankManager.class, "change_desc") +
-                            "\n\n" +
-                            Messages.get(RankManager.class, "cancel_warn"),
-                    Messages.get(Rankable.class, "rank1"),
-                    Messages.get(Rankable.class, "rank2"),
-                    Messages.get(Rankable.class, "rank3"),
-                    Messages.get(RankManager.class, "cancel"));
-            this.rankedItem = item;
-        }
-
-        @Override
-        protected void onSelect(int index) {
-            if (index < 3) {
-                ((Rankable)rankedItem).rank(index+1);
-                GLog.p(Messages.get(RankManager.class, "switch", Rankable.getRankString(index+1)));
-                Invisibility.dispel();
-                curUser.spend( 1f );
-                curUser.busy();
-                ((HeroSprite)curUser.sprite).read();
-
-                Sample.INSTANCE.play( Assets.Sounds.READ );
-                curUser.sprite.burst(Rankable.getRankColor(index+1), 10);
-                Enchanting.show(curUser, new Item(){
-                    @Override
-                    public int image() {
-                        return rankedItem.image();
-                    }
-
-                    @Override
-                    public ItemSprite.Glowing glowing() {
-                        return new ItemSprite.Glowing(Rankable.getRankColor(index+1));
-                    }
-                });
-                updateQuickslot();
-            }
-
-            rankedItem = null;
-        }
-
-        @Override
-        protected boolean hasInfo(int index) {
-            return index < 3;
-        }
-
-        @Override
-        protected void onInfo( int index ) {
-            GameScene.show(new WndTitledMessage(
-                    Icons.get(Icons.INFO),
-                    Messages.titleCase(Messages.get(Rankable.class, "rank" + (index+1))),
-                    ((Rankable)rankedItem).getRankMessage(index+1)));
-        }
-
-        @Override
-        public void onBackPressed() {
-            //do nothing, reader has to cancel
-        }
-
-    }
 
     @Override
     public boolean isFaceProtected() {
---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndRankManager.java
@@ -0,0 +1,176 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2024 Evan Debenham
+ *
+ * Summoning Pixel Dungeon Reincarnated
+ * Copyright (C) 2023-2025 Trashbox Bobylev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.windows;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Invisibility;
+import com.shatteredpixel.shatteredpixeldungeon.effects.Enchanting;
+import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.items.Rankable;
+import com.shatteredpixel.shatteredpixeldungeon.items.spells.RankManager;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.PixelScene;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.HeroSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.shatteredpixel.shatteredpixeldungeon.ui.RedButton;
+import com.shatteredpixel.shatteredpixeldungeon.ui.RenderedTextBlock;
+import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
+import com.watabou.noosa.audio.Sample;
+
+import java.util.ArrayList;
+
+public class WndRankManager extends WndTabbed {
+
+    protected static final int WIDTH_MIN    = 120;
+    protected static final int WIDTH_MAX    = 220;
+    protected static final int GAP	= 2;
+    protected static final int BUTTON_HEIGHT = 16;
+
+    private ArrayList<RenderedTextBlock> texts = new ArrayList<>();
+    private RedButton btnSwitch;
+
+    private static Item rankedItem;
+    private static Item ranker;
+    protected static int selectedRank;
+
+    public WndRankManager(RankManager rankManager, Item item){
+        super();
+
+        rankedItem = item;
+        ranker = rankManager;
+
+        int width = WIDTH_MIN;
+
+        IconTitle titlebar = new IconTitle( new ItemSprite(item), Messages.titleCase(Messages.get(this, "rank", item.trueName())) );
+        titlebar.setRect( 0, 0, width, 0 );
+        add(titlebar);
+
+        RenderedTextBlock largest = null;
+        for (int i = 0; i < 3; i++){
+            RenderedTextBlock text = PixelScene.renderTextBlock( 6 );
+            text.text( Messages.get(RankManager.class, "change_desc") + "\n\n"
+                    + ((Rankable)rankedItem).getRankMessage(i+1), width );
+            text.setPos( titlebar.left(), titlebar.bottom() + 2*GAP );
+            add( text );
+            texts.add(text);
+
+            if (largest == null || text.height() > largest.height()){
+                largest = text;
+            }
+
+            int finalI = i;
+            add(new LabeledTab(Messages.get(Rankable.class, "rank" + (finalI + 1))){
+                @Override
+                protected void select(boolean value) {
+                    super.select( value );
+                    texts.get(finalI).visible = value;
+                }
+            });
+        }
+
+        while (PixelScene.landscape()
+                && largest.bottom() > (PixelScene.MIN_HEIGHT_L - 20 - BUTTON_HEIGHT)
+                && width < WIDTH_MAX){
+            width += 20;
+            titlebar.setRect(0, 0, width, 0);
+
+            largest = null;
+            for (RenderedTextBlock text : texts){
+                text.setPos( titlebar.left(), titlebar.bottom() + 2*GAP );
+                text.maxWidth(width);
+                if (largest == null || text.height() > largest.height()){
+                    largest = text;
+                }
+            }
+        }
+
+        bringToFront(titlebar);
+
+        btnSwitch = new RedButton( Messages.get(this, "switch_rank") ) {
+            @Override
+            protected void onClick() {
+                hide();
+                ((Rankable)rankedItem).rank(selectedRank);
+                GLog.p(Messages.get(RankManager.class, "switch", Rankable.getRankString(selectedRank)));
+                Invisibility.dispel();
+                Dungeon.hero.spend( 1f );
+                Dungeon.hero.busy();
+                ((HeroSprite)Dungeon.hero.sprite).read();
+
+                Sample.INSTANCE.play( Assets.Sounds.READ );
+                Dungeon.hero.sprite.burst(Rankable.getRankColor(selectedRank), 10);
+                ranker.detach(Dungeon.hero.belongings.backpack);
+                Enchanting.show(Dungeon.hero, new Item(){
+                    @Override
+                    public int image() {
+                        return rankedItem.image();
+                    }
+
+                    @Override
+                    public ItemSprite.Glowing glowing() {
+                        return new ItemSprite.Glowing(Rankable.getRankColor(selectedRank));
+                    }
+                });
+                Item.updateQuickslot();
+
+                rankedItem = null;
+                ranker = null;
+            }
+        };
+        btnSwitch.setRect(0, largest.bottom()+2*GAP, width, BUTTON_HEIGHT);
+        add(btnSwitch);
+
+        btnSwitch.icon(new ItemSprite(ItemSpriteSheet.RANK_MANAGER));
+        btnSwitch.enable(Dungeon.hero.ready);
+
+        resize( width, (int)btnSwitch.bottom() + 2 );
+
+        layoutTabs();
+        selectedRank = ((Rankable) rankedItem).rank();
+        select(selectedRank-1);
+    }
+
+    @Override
+    public void select(Tab tab) {
+        super.select(tab);
+        selectedRank = tabs.indexOf(tab)+1;
+    }
+
+    @Override
+    public synchronized void update() {
+        super.update();
+        if (!btnSwitch.active && Dungeon.hero.ready){
+            btnSwitch.enable(((Rankable)rankedItem).rank() == (tabs.indexOf(selected)+1));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        ((RankManager)ranker).reShowSelector();
+    }
+}
