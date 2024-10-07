df9eb0399a78b293751c7a1401a269f60b94302e
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2024-10-07 23:59:44 +0700

    ReInc 0.4: fixed staffs not being in magical holster

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/bags/MagicalHolster.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/bags/MagicalHolster.java
@@ -27,6 +27,7 @@ package com.shatteredpixel.shatteredpixeldungeon.items.bags;
 import com.shatteredpixel.shatteredpixeldungeon.items.Item;
 import com.shatteredpixel.shatteredpixeldungeon.items.bombs.Bomb;
 import com.shatteredpixel.shatteredpixeldungeon.items.bombs.ElectricalExplosive;
+import com.shatteredpixel.shatteredpixeldungeon.items.staffs.Staff;
 import com.shatteredpixel.shatteredpixeldungeon.items.wands.Wand;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.MissileWeapon;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
@@ -42,7 +43,7 @@ public class MagicalHolster extends Bag {
 	
 	@Override
 	public boolean canHold( Item item ) {
-		if (item instanceof Wand || item instanceof MissileWeapon || item instanceof Bomb){
+		if (item instanceof Wand || item instanceof MissileWeapon || item instanceof Bomb || item instanceof Staff){
 			return super.canHold(item);
 		} else {
 			return false;
