7c43670c440607c6a26348a8d1d4878eb88d4e5c
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2025-04-29 01:01:54 +0700

    ReInc 0.5.0: fixed ad-hoc spells not properly showing tier info window

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/magic/AdHocSpell.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/magic/AdHocSpell.java
@@ -61,6 +61,8 @@ public abstract class AdHocSpell extends ConjurerSpell {
                 updateQuickslot();
             }
 
+        } else {
+            super.execute(hero, action);
         }
     }
 }
