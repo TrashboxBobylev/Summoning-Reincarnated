38f731a258806b6b5531cf51c39759db2d0a20f1
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2025-05-18 22:20:07 +0700

    ReInc 0.5.0: shop pricing now considers scaling depth

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/Shopkeeper.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/Shopkeeper.java
@@ -204,7 +204,7 @@ public class Shopkeeper extends NPC {
 
 	//shopkeepers are greedy!
 	public static int sellPrice(Item item){
-		return item.value() * 5 * (Dungeon.depth / 5 + 1);
+		return item.value() * 5 * (Dungeon.scalingDepth() / 5 + 1);
 	}
 	
 	public static WndBag sell() {
