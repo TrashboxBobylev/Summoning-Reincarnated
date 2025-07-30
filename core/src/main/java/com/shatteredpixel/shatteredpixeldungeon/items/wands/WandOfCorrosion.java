8de30f1122eab0bdb1f96cf4794a5748210d4675
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2025-07-31 04:04:34 +0700

    ReInc 0.6: implemented ranks for wand of corrosion

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/WandOfCorrosion.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/WandOfCorrosion.java
@@ -33,6 +33,7 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.Blob;
 import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.CorrosiveGas;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Corrosion;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Ooze;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.DwarfKing;
 import com.shatteredpixel.shatteredpixeldungeon.effects.CellEmitter;
@@ -43,7 +44,10 @@ import com.shatteredpixel.shatteredpixeldungeon.mechanics.Ballistica;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.shatteredpixel.shatteredpixeldungeon.ui.BuffIndicator;
+import com.watabou.noosa.Image;
 import com.watabou.noosa.audio.Sample;
+import com.watabou.utils.Bundle;
 import com.watabou.utils.Callback;
 import com.watabou.utils.ColorMath;
 import com.watabou.utils.PathFinder;
@@ -57,14 +61,49 @@ public class WandOfCorrosion extends Wand {
 		collisionProperties = Ballistica.STOP_TARGET | Ballistica.STOP_SOLID;
 	}
 
-	@Override
+    @Override
+    public float rechargeModifier(int rank) {
+        switch (rank){
+            case 1: return 2.0f;
+            case 2: return 0.8f;
+            case 3: return 3.5f;
+        }
+        return super.rechargeModifier(rank);
+    }
+
+    public int amountOfGas(int rank){
+        switch (rank){
+            case 1: return (int) (50 + 10 * power());
+            case 2: return (int) (60 + 12 * power());
+            case 3: return (int) (1000 + 200*power());
+        }
+        return 1;
+    }
+
+    public int gasPower(int rank){
+        switch (rank){
+            case 1: return Math.round(2 + power());
+            case 2: return Math.round(3 + power());
+            case 3: return Math.round(1 + power()/2);
+        }
+        return 1;
+    }
+
+    @Override
 	public void onZap(Ballistica bolt) {
-		CorrosiveGas gas = Blob.seed(bolt.collisionPos, (int) (50 + 10 * power()), CorrosiveGas.class);
-		CellEmitter.get(bolt.collisionPos).burst(Speck.factory(Speck.CORROSION), 10 );
-		gas.setStrength(Math.round(2 + power()), getClass());
-		if (Dungeon.isChallenged(Conducts.Conduct.PACIFIST))
-			gas.setStrength(gas.volume/3);
-		GameScene.add(gas);
+        if (rank() == 2){
+            Char ch = Actor.findChar(bolt.collisionPos);
+            if (ch != null){
+                Buff.affect(ch, CorrosiveImbue.class).set(gasPower(rank()), amountOfGas(rank()));
+            }
+        } else {
+            CorrosiveGas gas = Blob.seed(bolt.collisionPos, amountOfGas(rank()), CorrosiveGas.class);
+            gas.setStrength(gasPower(rank()), getClass());
+            if (Dungeon.isChallenged(Conducts.Conduct.PACIFIST))
+                gas.setStrength(gas.volume/3);
+            GameScene.add(gas);
+        }
+        CellEmitter.get(bolt.collisionPos).burst(Speck.factory(Speck.CORROSION), 10 );
 		Sample.INSTANCE.play(Assets.Sounds.GAS);
 
 		for (int i : PathFinder.NEIGHBOURS9) {
@@ -124,11 +163,8 @@ public class WandOfCorrosion extends Wand {
 
 	@Override
 	public String statsDesc() {
-		if (levelKnown)
-			return Messages.get(this, "stats_desc", (int)(2+power()));
-		else
-			return Messages.get(this, "stats_desc", 2);
-	}
+        return Messages.get(this, "stats_desc" + rank(), gasPower(rank()));
+    }
 
 	@Override
 	public String upgradeStat1(int level) {
@@ -139,4 +175,84 @@ public class WandOfCorrosion extends Wand {
 	public String upgradeStat2(int level) {
 		return Messages.decimalFormat("#.##x", 1+.2f*level);
 	}
+
+    @Override
+    public String getRankMessage(int rank) {
+        return Messages.get(this, "rank" + rank,
+                getRechargeInfo(rank),
+                gasPower(rank), amountOfGas(rank)
+        );
+    }
+
+    public static class CorrosiveImbue extends Buff {
+        {
+            type = buffType.NEGATIVE;
+            announced = true;
+        }
+
+        public int power = 0;
+        public int burst = 0;
+
+        private static final String POWER = "power";
+        private static final String BURST = "nurst";
+
+        @Override
+        public void storeInBundle( Bundle bundle ) {
+            super.storeInBundle( bundle );
+            bundle.put(POWER, power );
+            bundle.put(BURST, burst );
+        }
+
+        @Override
+        public void restoreFromBundle( Bundle bundle ) {
+            super.restoreFromBundle( bundle );
+            power = bundle.getInt(POWER);
+            burst = bundle.getInt(BURST);
+        }
+
+        public void set(int power, int burst){
+            this.power = power;
+            this.burst = burst;
+        }
+
+        @Override
+        public int icon() {
+            return BuffIndicator.POISON;
+        }
+
+        @Override
+        public void tintIcon(Image icon) {
+            icon.hardlight(1f, 0.5f, 0f);
+        }
+
+        @Override
+        public void fx(boolean on) {
+            if (on) target.sprite.aura( 0xce7a4c, 6 );
+            else target.sprite.clearAura();
+        }
+
+        @Override
+        public void detach() {
+            super.detach();
+            if (!target.isAlive()){
+                CorrosiveGas gas = Blob.seed(target.pos, burst, CorrosiveGas.class);
+                CellEmitter.get(target.pos).burst(Speck.factory(Speck.CORROSION), 10 );
+                gas.setStrength(power, WandOfCorrosion.class);
+                if (Dungeon.isChallenged(Conducts.Conduct.PACIFIST))
+                    gas.setStrength(gas.volume/3);
+                GameScene.add(gas);
+                if (target.sprite != null && target.sprite.visible)
+                    Sample.INSTANCE.play(Assets.Sounds.GAS);
+            }
+        }
+
+        @Override
+        public String desc() {
+            return Messages.get(this, "desc", power, burst);
+        }
+
+        {
+            immunities.add(Corrosion.class);
+        }
+    }
 }
