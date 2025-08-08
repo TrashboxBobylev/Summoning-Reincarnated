81da20e66d2224376d565e9803968a56fb599491
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2025-08-09 01:37:44 +0700

    ReInc 0.6: reimplemented bomb maze room:
    - now features all kinds of enhanced bombs instead of just shrapnel and arcane nukes
    - increased amount of bombs by 1.5x
    - now show the explosive trap beneath each bomb

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/BoomRoom.java
@@ -0,0 +1,137 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2025 Evan Debenham
+ *
+ * Summoning Pixel Dungeon Reincarnated
+ * Copyright (C) 2023-2025 Trashbox Bobylev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.levels.rooms.standard;
+
+import com.shatteredpixel.shatteredpixeldungeon.items.Heap;
+import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.items.bombs.ArcaneBomb;
+import com.shatteredpixel.shatteredpixeldungeon.items.bombs.ChaoticBomb;
+import com.shatteredpixel.shatteredpixeldungeon.items.bombs.Firebomb;
+import com.shatteredpixel.shatteredpixeldungeon.items.bombs.FrostBomb;
+import com.shatteredpixel.shatteredpixeldungeon.items.bombs.HolyBomb;
+import com.shatteredpixel.shatteredpixeldungeon.items.bombs.Noisemaker;
+import com.shatteredpixel.shatteredpixeldungeon.items.bombs.RegrowthBomb;
+import com.shatteredpixel.shatteredpixeldungeon.items.bombs.ShrapnelBomb;
+import com.shatteredpixel.shatteredpixeldungeon.items.bombs.ShrinkingBomb;
+import com.shatteredpixel.shatteredpixeldungeon.items.bombs.SmokeBomb;
+import com.shatteredpixel.shatteredpixeldungeon.items.bombs.Webbomb;
+import com.shatteredpixel.shatteredpixeldungeon.items.bombs.WoollyBomb;
+import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
+import com.shatteredpixel.shatteredpixeldungeon.levels.Terrain;
+import com.shatteredpixel.shatteredpixeldungeon.levels.features.Maze;
+import com.shatteredpixel.shatteredpixeldungeon.levels.painters.Painter;
+import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.Room;
+import com.shatteredpixel.shatteredpixeldungeon.levels.traps.ExplosiveTrap;
+import com.watabou.utils.PathFinder;
+import com.watabou.utils.Point;
+import com.watabou.utils.Random;
+import com.watabou.utils.Reflection;
+
+public class BoomRoom extends StandardRoom {
+
+    @Override
+    public int minWidth() {
+        return 28;
+    }
+
+    @Override
+    public int minHeight() {
+        return 28;
+    }
+
+    @Override
+    public int maxWidth() {
+        return 28;
+    }
+
+    @Override
+    public int maxHeight() {
+        return 28;
+    }
+
+    {
+        noMobs = true;
+    }
+
+    @Override
+    public boolean canMerge(Level l, Room other, Point p, int mergeTerrain) {
+        return false;
+    }
+
+    @Override
+    public void paint(Level level) {
+
+        Painter.fill(level, this, Terrain.WALL);
+        Painter.fill(level, this, 1, Terrain.EMBERS);
+
+        //true = space, false = wall
+        Maze.allowDiagonals = false;
+        boolean[][] maze = Maze.generate(this);
+        boolean[] passable = new boolean[width()*height()];
+
+        Painter.fill(level, this, 1, Terrain.EMPTY);
+        for (int x = 0; x < maze.length; x++) {
+            for (int y = 0; y < maze[0].length; y++) {
+                if (maze[x][y] == Maze.FILLED) {
+                    Painter.fill(level, x + left, y + top, 1, 1, Terrain.WALL_DECO);
+                }
+                passable[x + width()*y] = maze[x][y] == Maze.EMPTY;
+            }
+        }
+
+        PathFinder.setMapSize(width(), height());
+        Point entrance = connected.values().iterator().next();
+        int entrancePos = (entrance.x - left) + width()*(entrance.y - top);
+
+        PathFinder.buildDistanceMap( entrancePos, passable );
+
+        for (int i = 0; i < 30; i++) {
+            int dropPos;
+            do {
+                dropPos = level.pointToCell(random());
+            } while (level.map[dropPos] != Terrain.EMPTY || level.heaps.get( dropPos ) != null);
+            Item prize = Reflection.newInstance(Random.oneOf(
+                    Firebomb.class,
+                    FrostBomb.class,
+                    RegrowthBomb.class,
+                    WoollyBomb.class,
+                    HolyBomb.class,
+                    Webbomb.class,
+                    Noisemaker.class,
+                    SmokeBomb.class,
+                    ShrinkingBomb.class,
+                    ChaoticBomb.class,
+                    ArcaneBomb.class,
+                    ArcaneBomb.class,
+                    ShrapnelBomb.class
+            ));
+            level.drop(prize, dropPos).type = Heap.Type.HEAP;
+            level.map[dropPos] = Terrain.TRAP;
+            level.setTrap(new ExplosiveTrap().reveal(), dropPos);
+        }
+
+        PathFinder.setMapSize(level.width(), level.height());
+    }
+}
--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/StandardRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/StandardRoom.java
@@ -168,6 +168,7 @@ public abstract class StandardRoom extends Room {
 		rooms.add(ChasmRoom.class);
 		rooms.add(SkullsRoom.class);
 		rooms.add(RitualRoom.class);
+        rooms.add(BoomRoom.class);
 
 
 		rooms.add(PlantsRoom.class);
@@ -185,21 +186,21 @@ public abstract class StandardRoom extends Room {
 	private static float[][] chances = new float[27][];
 
     static {
-		chances[1] =  new float[]{16,8,8,4,4,2,   0,0,0,0,0, 0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,  1,0,1,0,1,0,1,1,0,0};
-		chances[2] =  new float[]{16,8,8,4,4,2,   0,0,0,0,0, 0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,  1,1,1,1,1,1,1,1,1,1};
+		chances[1] =  new float[]{16,8,8,4,4,2,   0,0,0,0,0, 0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,  1,0,1,0,1,0,1,1,0,0};
+		chances[2] =  new float[]{16,8,8,4,4,2,   0,0,0,0,0, 0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,  1,1,1,1,1,1,1,1,1,1};
 		chances[4] =  chances[3] = chances[2];
-		chances[5] =  new float[]{16,8,8,4,0,0,    0,0,0,0,0, 0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0};
+		chances[5] =  new float[]{16,8,8,4,0,0,    0,0,0,0,0, 0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0};
 
-		chances[6] =  new float[]{0,0,0,0,0,0, 10,10,10,5,5, 0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,  1,1,1,1,1,1,1,1,1,1};
+		chances[6] =  new float[]{0,0,0,0,0,0, 10,10,10,5,5, 0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,  1,1,1,1,1,1,1,1,1,1};
 		chances[10] = chances[9] = chances[8] = chances[7] = chances[6];
 
-		chances[11] = new float[]{0,0,0,0,0,0, 0,0,0,0,0, 16,8,8,4,4,2,   0,0,0,0,0, 0,0,0,0,0,  1,1,1,1,1,1,1,1,1,1};
+		chances[11] = new float[]{0,0,0,0,0,0, 0,0,0,0,0, 16,8,8,4,4,2,   0,0,0,0,0, 0,0,0,0,0,0,  1,1,1,1,1,1,1,1,1,1};
 		chances[15] = chances[14] = chances[13] = chances[12] = chances[11];
 
-		chances[16] = new float[]{0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0, 10,10,10,5,5, 0,0,0,0,0,  1,1,1,1,1,1,1,1,1,1};
+		chances[16] = new float[]{0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0, 10,10,10,5,5, 0,0,0,0,0,0,  1,1,1,1,1,1,1,1,1,1};
 		chances[20] = chances[19] = chances[18] = chances[17] = chances[16];
 
-		chances[21] = new float[]{0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0, 0,0,0,0,0, 10,10,10,5,5,  1,1,1,1,1,1,1,1,1,1};
+		chances[21] = new float[]{0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0, 0,0,0,0,0, 10,10,10,5,5,2,  1,1,1,1,1,1,1,1,1,1};
 		chances[26] = chances[25] = chances[24] = chances[23] = chances[22] = chances[21];
 
     }
