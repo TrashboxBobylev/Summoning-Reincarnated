1ff297db9e2c80f056b15b90242e04d4e1734b15
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2025-08-14 03:22:20 +0700

    ReInc 0.6: implemented ranks for force cube

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/missiles/ForceCube.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/missiles/ForceCube.java
@@ -31,10 +31,12 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.items.wands.WandOfBlastWave;
 import com.shatteredpixel.shatteredpixeldungeon.levels.traps.TenguDartTrap;
+import com.shatteredpixel.shatteredpixeldungeon.mechanics.Ballistica;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
 import com.watabou.noosa.audio.Sample;
+import com.watabou.utils.BArray;
 import com.watabou.utils.PathFinder;
 
 import java.util.ArrayList;
@@ -44,17 +46,35 @@ public class ForceCube extends MissileWeapon {
 	{
 		image = ItemSpriteSheet.FORCE_CUBE;
 		
-		tier = 5;
-		baseUses = 5;
-		
 		sticky = false;
 	}
 
-	@Override
-	public int max(int lvl) {
-		return  4 * tier +                  //20 base, down from 25
-				(tier) * lvl;               //scaling unchanged
-	}
+    public float min(float lvl, int rank) {
+        switch (rank){
+            case 1: return 4 + lvl;
+            case 2: return 3 + lvl*0.67f;
+            case 3: return 1 + lvl*1.5f;
+        }
+        return 0;
+    }
+
+    public float max(float lvl, int rank) {
+        switch (rank){
+            case 1: return 8 + lvl*2.5f;
+            case 2: return 6 + lvl*1.75f;
+            case 3: return 20 + lvl*8;
+        }
+        return 0;
+    }
+
+    public float baseUses(float lvl, int rank){
+        switch (rank){
+            case 1: return 5 + lvl*1.25f;
+            case 2: return 3 + lvl*1f;
+            case 3: return 1 + lvl*0.25f;
+        }
+        return 1;
+    }
 
 	@Override
 	public void hitSound(float pitch) {
@@ -86,11 +106,21 @@ public class ForceCube extends MissileWeapon {
 		
 		ArrayList<Char> targets = new ArrayList<>();
 		if (Actor.findChar(cell) != null) targets.add(Actor.findChar(cell));
-		
-		for (int i : PathFinder.NEIGHBOURS8){
-			if (!(Dungeon.level.traps.get(cell+i) instanceof TenguDartTrap)) Dungeon.level.pressCell(cell+i);
-			if (Actor.findChar(cell + i) != null) targets.add(Actor.findChar(cell + i));
-		}
+
+        if (rank() != 3){
+            for (int i : PathFinder.NEIGHBOURS8){
+                if (!(Dungeon.level.traps.get(cell+i) instanceof TenguDartTrap)) Dungeon.level.pressCell(cell+i);
+                if (Actor.findChar(cell + i) != null) targets.add(Actor.findChar(cell + i));
+            }
+        } else {
+            PathFinder.buildDistanceMap( cell, BArray.not( Dungeon.level.solid, null ), 3 );
+            for (int i = 0; i < PathFinder.distance.length; i++) {
+                if (PathFinder.distance[i] < Integer.MAX_VALUE) {
+                    if (!(Dungeon.level.traps.get(i) instanceof TenguDartTrap)) Dungeon.level.pressCell(i);
+                    if (Actor.findChar(i) != null) targets.add(Actor.findChar(i));
+                }
+            }
+        }
 		
 		for (Char target : targets){
 			curUser.shoot(target, this);
@@ -99,9 +129,34 @@ public class ForceCube extends MissileWeapon {
 				Dungeon.fail(this);
 				GLog.n(Messages.get(this, "ondeath"));
 			}
+            if (rank() == 2){
+                //do not push chars that are dieing over a pit, or that move due to the damage
+                if ((target.isAlive() || target.flying || !Dungeon.level.pit[target.pos])) {
+                    //trace a ballistica to our target (which will also extend past them
+                    Ballistica trajectory = new Ballistica(cell, target.pos, Ballistica.STOP_TARGET);
+                    //trim it to just be the part that goes past them
+                    trajectory = new Ballistica(trajectory.collisionPos, trajectory.path.get(trajectory.path.size()-1), Ballistica.PROJECTILE);
+                    int strength = 2 + (int) powerLevel();
+                    WandOfBlastWave.throwChar(target, trajectory, strength, false, true, this);
+                }
+            }
 		}
 		
 		WandOfBlastWave.BlastWave.blast(cell);
+        if (rank() == 3){
+            for (int i : PathFinder.NEIGHBOURS8){
+                WandOfBlastWave.BlastWave.blast(cell+i);
+            }
+            Sample.INSTANCE.play( Assets.Sounds.BLAST, 1.5f, 0.5f );
+        }
 		Sample.INSTANCE.play( Assets.Sounds.BLAST );
 	}
+
+    @Override
+    public int proc(Char attacker, Char defender, int damage) {
+        if (rank() == 3){
+            damage += defender.drRoll();
+        }
+        return super.proc(attacker, defender, damage);
+    }
 }
