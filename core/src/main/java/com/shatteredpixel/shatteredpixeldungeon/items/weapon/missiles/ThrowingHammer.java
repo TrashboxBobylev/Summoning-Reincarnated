b2cf52525b0266a8f0ffc3f0ea71e99db5ddaf7d
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2025-08-14 02:28:02 +0700

    ReInc 0.6: implemented ranks for throwing hammer

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/missiles/ThrowingHammer.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/missiles/ThrowingHammer.java
@@ -25,6 +25,10 @@
 package com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles;
 
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Daze;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Paralysis;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 
 public class ThrowingHammer extends MissileWeapon {
@@ -33,15 +37,55 @@ public class ThrowingHammer extends MissileWeapon {
 		image = ItemSpriteSheet.THROWING_HAMMER;
 		hitSound = Assets.Sounds.HIT_CRUSH;
 		hitSoundPitch = 0.8f;
-		
-		tier = 5;
-		baseUses = 12;
+
 		sticky = false;
 	}
-	
-	@Override
-	public int max(int lvl) {
-		return  4 * tier +                  //20 base, down from 25
-				(tier) * lvl;               //scaling unchanged
-	}
+
+    @Override
+    public float castDelay(Char user, int cell) {
+        float delay = super.castDelay(user, cell);
+        if (rank() == 3) {
+            delay /= 3f;
+        }
+        return delay;
+    }
+
+    public float min(float lvl, int rank) {
+        switch (rank){
+            case 1: return 4 + lvl;
+            case 2: return 1 + lvl*0.4f;
+            case 3: return 0 + lvl*0.2f;
+        }
+        return 0;
+    }
+
+    public float max(float lvl, int rank) {
+        switch (rank){
+            case 1: return 8 + lvl*2f;
+            case 2: return 5 + lvl*1.25f;
+            case 3: return 3 + lvl*1f;
+        }
+        return 0;
+    }
+
+    public float baseUses(float lvl, int rank){
+        switch (rank){
+            case 1: return 12 + lvl*2.5f;
+            case 2: return 16 + lvl*3.25f;
+            case 3: return 20 + lvl*4f;
+        }
+        return 1;
+    }
+
+    @Override
+    public int proc(Char attacker, Char defender, int damage) {
+        if (rank() == 2){
+            Buff.affect(defender, Daze.class, 5f);
+        }
+        if (rank() == 3){
+            Buff.prolong(defender, Paralysis.class, 2f);
+            damage += defender.drRoll();
+        }
+        return super.proc(attacker, defender, damage);
+    }
 }
