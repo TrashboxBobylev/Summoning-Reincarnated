18c093b283fba23a76b8bd800baaf2178d53bdde
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-08-19 01:50:08 +0700

    ReInc 0.7.2: fixed crash on showing a target from invisible sprite

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Mob.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Mob.java
@@ -1424,7 +1424,7 @@ public abstract class Mob extends Char {
 	}
 
 	public void showCurrentTarget(){
-		if (enemy != null && enemy.sprite != null && sprite.visible && enemy.sprite.visible){
+		if (enemy != null && enemy.sprite != null && sprite != null && sprite.visible && enemy.sprite.visible){
 			MagicMissile m = MagicMissile.boltFromChar(sprite.parent,
 					MagicMissile.TARGET,
 					sprite,
