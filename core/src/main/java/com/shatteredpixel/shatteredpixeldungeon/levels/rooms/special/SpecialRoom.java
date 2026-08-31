542bcfebc41a4b1fac91ed2badb59a30608bf17d
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-08-31 21:46:26 +0700

    ReInc 0.7.2: fixed mystery merchant room replacing all special rooms in project paradox

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/special/SpecialRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/special/SpecialRoom.java
@@ -151,7 +151,7 @@ public abstract class SpecialRoom extends Room {
 	
 	private static void useType( Class<?extends Room> type ) {
 		if (Dungeon.mode == Dungeon.GameMode.CHAOS &&
-				(type != AttunementPotRoom.class && type != LaboratoryRoom.class && type != PitRoom.class)){
+				(type != AttunementPotRoom.class && type != LaboratoryRoom.class && type != PitRoom.class && type != MysteryMerchantRoom.class)){
 			//deck? what's that
 		} else {
 			floorSpecials.remove(type);
